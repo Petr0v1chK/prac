ccc8bd85607ace8dfd6d4968a52aa61add6c841e
Куркуль <gosha_ku90@mail.ru>
2026-04-23 01:17:53 +0500

    Delete ladno directory

--- ladno/includes/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$connect = new mysqli("localhost", "root", "", "db_demo_2025");
-if ($connect->connect_error) {
-    die("Ошибка подключения: " . $connect->connect_error);
-}
-$connect->set_charset("utf8mb4");
-?>
